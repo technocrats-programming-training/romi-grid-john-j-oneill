b0a8dee6bfc64bb0fcc363a38f4608d33b55681e
John O'Neill <38105550+john-j-oneill@users.noreply.github.com>
2022-06-30 23:13:07 +0000

    Code that I know does not work!

--- src/main/java/frc/robot/Position.java
+++ src/main/java/frc/robot/Position.java
@@ -2,31 +2,47 @@ package frc.robot;
 
 import static lib.Romi.driveDistance;
 import static lib.Romi.turnDegrees;
+import java.util.*;
 
 public class Position {
+    public double positionInchesX = 0;
+    public double positionInchesY = 0;
+    public double positionYawDegrees = 90;
+    public double distanceToDriveInches = 10;
 
+    public void driveAngle(double targetYawDegrees, double driveDistanceInches) throws InterruptedException {
+      double targetYawRadians = Math.toRadians(targetYawDegrees);
+      double correctionYawDegrees = targetYawDegrees - positionYawDegrees;
+      turnDegrees(correctionYawDegrees);
+      positionYawDegrees = targetYawDegrees;
+      driveDistance(driveDistanceInches);
+      positionInchesY += Math.sin(targetYawRadians) * driveDistanceInches;
+      positionInchesX += Math.cos(targetYawRadians) * driveDistanceInches;
+      
+    }
 
     public void driveUp() throws InterruptedException {
-
+      driveAngle(90,distanceToDriveInches);
     }
 
     public void driveDown() throws InterruptedException {
-
+      driveAngle(-90,distanceToDriveInches);
     }
 
     public void driveRight() throws InterruptedException {
-
+      driveAngle(0,distanceToDriveInches);
     }
 
     public void driveLeft() throws InterruptedException {
-
+      driveAngle(180,distanceToDriveInches);
     }
 
     public void returnToHome() {
-
+      //double angleToHome = Math.atan2(-y,-x);
+      //driveAngle(Math.toDegrees(angleToHome));
     }
 
     public String toString() {
-        return "";
+        return positionInchesX + "," + positionInchesY;
     }
 }
